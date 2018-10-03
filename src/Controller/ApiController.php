78080c8d789b43037889d8a00d1c9e173f61394c
Alexandre Sciacca <alexandre.sciaca@gmail.com>
2018-10-03 15:23:28 +0200

    Ajour DownLoadAction

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -9,20 +9,45 @@
 namespace App\Controller;
 
 
-use App\Entity\ContactRequest;
-use App\Form\ContactForm;
-use ScyLabs\NeptuneBundle\Entity\Infos;
-use ScyLabs\NeptuneBundle\Entity\User;
+use ScyLabs\NeptuneBundle\Entity\Document;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends Controller
 {
+    /**
+     * @Route("/downloads/{name}{id}.{ext}",name="api_download",requirements={"id"="\d+","name"=".*"})
+     */
+    public function downloadAction(Request $request,$id,$name,$ext){
+
+        $doc = $this->getDoctrine()->getRepository(Document::class)->find($id);
+
+        if($request->headers->has('referer'))
+            $redirect =  $this->redirect($request->headers->get('referer'));
+        else
+            $redirect =  $this->redirectToRoute('homepage');
+
+        if($doc === null){
+            return $redirect;
+        }
+
+        if(!file_exists($this->getParameter('uploads_directory').$doc->getPath())){
+            return $redirect;
+        }
+        $path = $this->getParameter('uploads_directory').$doc->getPath();
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . $name.$id.'.'.$ext);
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($path));
+
+        return new Response(readfile($path));
+    }
 
 }
