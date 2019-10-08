c770199a9d5308c30c929c2ff46b49aff883ece5
Alexandre Sciacca <alexandre.sciacca@gmail.com>
2019-10-08 09:43:14 +0200

    Edit DownloadAction

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -13,12 +13,13 @@ use ScyLabs\NeptuneBundle\Entity\Document;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends Controller
 {
     /**
-     * @Route("/downloads/{name}{id}.{ext}",name="api_download",requirements={"id"="\d+","name"=".*"})
+     * @Route("/downloads/{id}/{name}",name="api_download",requirements={"id"="\d+","name"=".*"})
      */
     public function downloadAction(Request $request,$id,$name,$ext){
 
@@ -38,16 +39,8 @@ class ApiController extends Controller
         }
         $path = $this->getParameter('uploads_directory').$doc->getPath();
 
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename=' . $name.$id.'.'.$ext);
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($path));
+        return $this->file($path,$name,ResponseHeaderBag::DISPOSITION_INLINE);
 
-        return new Response(readfile($path));
     }
 
 }
